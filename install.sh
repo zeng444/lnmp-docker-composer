71fad2d94043b0cf381a7a7741aa8fd732ff08dd
Robert <zeng444@163.com>
2019-08-12 22:55:52 +0800

    初始化

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+declare network=janfish2
+declare pwd=`pwd`
+declare pn=${1:-lnmp}
+if [ "`docker network ls | grep $network | awk '{print $2}'`" == "" ] ; then
+   docker network create $network
+fi
+docker-compose -p $pn -f $pwd/lnmp72/docker-compose.yml up --build -d
+echo
+echo "======================== your bootstrap script: ========================"
+echo
+echo " docker-compose -p $pn -f $pwd/lnmp72/docker-compose.yml up -d"
+echo
+echo "========================================================================"
