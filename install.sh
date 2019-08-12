ba68c5cdfb8caf0b86fe92e112483d7778e8878f
Robert <zeng444@163.com>
2019-08-12 23:27:02 +0800

    update README.MD

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-declare network=janfish2
+declare network=janfish
 declare pwd=`pwd`
 declare pn=${1:-lnmp}
-if [ "`docker network ls | grep $network | awk '{print $2}'`" == "" ] ; then
+if [ "`docker network ls | grep $network | awk '{print $2}'`" != "$network" ] ; then
    docker network create $network
 fi
 docker-compose -p $pn -f $pwd/lnmp72/docker-compose.yml up --build -d
